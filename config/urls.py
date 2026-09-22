5c321eb640ae6a38d03b29157005ce2eceebb641
Juan Contreras <142104016+luxury0101@users.noreply.github.com>
2026-09-21 23:26:54 -0500

    fix: servir imágenes multimedia en desarrollo

--- config/urls.py
+++ config/urls.py
@@ -39,3 +39,9 @@ urlpatterns = [
         name="swagger-ui",
     ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
